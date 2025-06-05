89c5293873fc0b2ecba87c46f79c236f46cde58b
Aakash Jangam <aakashhhjangam23@email.com>
2025-06-05 09:07:56 +0530

    first

---
+++ hello.js
@@ -0,0 +1,21 @@
+console.log("hello world!");
+
+let val=19;
+val='hello';
+val=false
+val='world'
+console.log("World")
+
+const pi = 3.14
+console.log(3.14)
+
+let fill = 75;
+if(fill<=50){
+    console.log("fill is above 50")
+
+}else if(fill <= 60){
+    console.log("fill is above 60")
+} else{
+    console.log("fill is above 75")
+
+}
